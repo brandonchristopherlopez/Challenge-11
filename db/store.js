72db73f52b87c128e72b26b93d8110a5d97e7ed5
Brandon_Lopez <brandonchristopherlopez@gmail.com>
2023-11-26 15:43:46 -0800

    css update

--- db/store.js
+++ db/store.js
@@ -1,7 +1,6 @@
 const util = require('util');
 const fs = require('fs');
 
-// This package will be used to generate our unique ids. https://www.npmjs.com/package/uuid
 const uuidv1 = require('uuid/v1');
 
 const readFileAsync = util.promisify(fs.readFile);
@@ -20,7 +19,6 @@ class Store {
     return this.read().then((notes) => {
       let parsedNotes;
 
-      // If notes isn't an array or can't be turned into one, send back a new empty array
       try {
         parsedNotes = [].concat(JSON.parse(notes));
       } catch (err) {
