0bd75d1aef45e95a55b7e961a29ec17ddf94e436
Brandon_Lopez <brandonchristopherlopez@gmail.com>
2023-11-28 14:01:18 -0800

    re-arranging file structure

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
